0a4e9d5bad2a24221a4daea83752701be8554ff4
Camilo V R <sebastianvallejoroldan@gmail.com>
2018-08-31 16:59:21 -0500

    fixed rate activities

--- app/src/main/java/com/restaurantapp/sebas/CalificarServicioActivity.java
+++ app/src/main/java/com/restaurantapp/sebas/CalificarServicioActivity.java
@@ -1,12 +1,13 @@
 package com.restaurantapp.sebas;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 
-public class CalificarServicioActivity extends AppCompatActivity {
+public class CalificarServicioActivity extends Activity {
 
     LinearLayout rating;
     CheckBox star;
